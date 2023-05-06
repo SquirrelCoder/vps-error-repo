0b894ef5afa0db36025685d3cb77871949cb6fe5
SquirrelCoder <SquirrelCoder@users.noreply.github.com>
2023-05-06 22:06:29 +0200

    Adding the condition that the code works

--- renderer/+onRenderHtml.ts
+++ renderer/+onRenderHtml.ts
@@ -1,31 +1,56 @@
 // https://vite-plugin-ssr.com/onRenderHtml
-import { renderToNodeStream } from '@vue/server-renderer'
-import { escapeInject } from 'vite-plugin-ssr/server'
+import { renderToString } from '@vue/server-renderer'
+import { dangerouslySkipEscape } from 'vite-plugin-ssr/server'
 import { createApp } from './app'
 import type { PageContext } from './types'
 import type { PageContextBuiltIn } from 'vite-plugin-ssr/types'
-import { getPageTags } from "./getPageTags";
 import { renderHeadToString } from "@vueuse/head";
 
 export default onRenderHtml
 
 async function onRenderHtml(pageContext: PageContextBuiltIn & PageContext) {
-  const { app, store, head } = createApp(pageContext);
-  const stream = await renderToNodeStream(app);
-  const initialStoreState = store.state.value;
-  const { title, description, robots } = getPageTags(pageContext);
-  const { headTags } = await renderHeadToString(head)
+  // the following snippet does not work
 
-  const documentHtml = escapeInject`<!DOCTYPE html>
+  // const { app, store, head } = createApp(pageContext);
+  // const stream = await renderToNodeStream(app);
+  // const initialStoreState = store.state.value;
+  // const { title, description, robots } = getPageTags(pageContext);
+  // const { headTags } = await renderHeadToString(head)
+  //
+  // const documentHtml = escapeInject`<!DOCTYPE html>
+  //   <html>
+  //     <head>
+  //       ${ headTags }
+  //     </head>
+  //     <body>
+  //       <div id="app">${ stream }</div>
+  //     </body>
+  //   </html>`;
+  //
+  // return {
+  //   documentHtml,
+  //   pageContext: {
+  //     enableEagerStreaming: true,
+  //     initialStoreState,
+  //   },
+  // };
+
+  // the following snippet does work correctly
+
+
+  const { app, store, head } = createApp(pageContext);
+  const { headTags } = await renderHeadToString(head)
+  const initialStoreState = store.state.value;
+  const appHTML = await renderToString(app)
+  const documentHtml = dangerouslySkipEscape(`<!DOCTYPE html>
     <html>
       <head>
-        ${ headTags }
-      </head>
+      ${ headTags }
+    <title>s</title></head>
       <body>
-        <div id="app">${ stream }</div>
+        <div id="app">${ appHTML }</div>
       </body>
-    </html>`;
-
+    </html>`);
   return {
     documentHtml,
     pageContext: {
