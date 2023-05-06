ba4892fec8df72af25c51ba965deea61e3912586
SquirrelCoder <SquirrelCoder@users.noreply.github.com>
2023-05-06 22:07:09 +0200

    Imports not removing

--- renderer/+onRenderHtml.ts
+++ renderer/+onRenderHtml.ts
@@ -1,56 +1,33 @@
 // https://vite-plugin-ssr.com/onRenderHtml
-import { renderToString } from '@vue/server-renderer'
-import { dangerouslySkipEscape } from 'vite-plugin-ssr/server'
+import { renderToNodeStream, renderToString } from '@vue/server-renderer'
+import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr/server'
 import { createApp } from './app'
 import type { PageContext } from './types'
 import type { PageContextBuiltIn } from 'vite-plugin-ssr/types'
 import { renderHeadToString } from "@vueuse/head";
+import { getPageTags } from "./getPageTags";
 
 export default onRenderHtml
 
 async function onRenderHtml(pageContext: PageContextBuiltIn & PageContext) {
   // the following snippet does not work
 
-  // const { app, store, head } = createApp(pageContext);
-  // const stream = await renderToNodeStream(app);
-  // const initialStoreState = store.state.value;
-  // const { title, description, robots } = getPageTags(pageContext);
-  // const { headTags } = await renderHeadToString(head)
-  //
-  // const documentHtml = escapeInject`<!DOCTYPE html>
-  //   <html>
-  //     <head>
-  //       ${ headTags }
-  //     </head>
-  //     <body>
-  //       <div id="app">${ stream }</div>
-  //     </body>
-  //   </html>`;
-  //
-  // return {
-  //   documentHtml,
-  //   pageContext: {
-  //     enableEagerStreaming: true,
-  //     initialStoreState,
-  //   },
-  // };
-
-  // the following snippet does work correctly
-
-
   const { app, store, head } = createApp(pageContext);
-  const { headTags } = await renderHeadToString(head)
+  const stream = await renderToNodeStream(app);
   const initialStoreState = store.state.value;
-  const appHTML = await renderToString(app)
-  const documentHtml = dangerouslySkipEscape(`<!DOCTYPE html>
+  const { title, description, robots } = getPageTags(pageContext);
+  const { headTags } = await renderHeadToString(head)
+
+  const documentHtml = escapeInject`<!DOCTYPE html>
     <html>
       <head>
-      ${ headTags }
-    <title>s</title></head>
+        ${ headTags }
+      </head>
       <body>
-        <div id="app">${ appHTML }</div>
+        <div id="app">${ stream }</div>
       </body>
-    </html>`);
+    </html>`;
+
   return {
     documentHtml,
     pageContext: {
@@ -58,4 +35,28 @@ async function onRenderHtml(pageContext: PageContextBuiltIn & PageContext) {
       initialStoreState,
     },
   };
+
+  // the following snippet does work correctly
+
+
+  // const { app, store, head } = createApp(pageContext);
+  // const { headTags } = await renderHeadToString(head)
+  // const initialStoreState = store.state.value;
+  // const appHTML = await renderToString(app)
+  // const documentHtml = dangerouslySkipEscape(`<!DOCTYPE html>
+  //   <html>
+  //     <head>
+  //     ${ headTags }
+  //   <title>s</title></head>
+  //     <body>
+  //       <div id="app">${ appHTML }</div>
+  //     </body>
+  //   </html>`);
+  // return {
+  //   documentHtml,
+  //   pageContext: {
+  //     enableEagerStreaming: true,
+  //     initialStoreState,
+  //   },
+  // };
 }
